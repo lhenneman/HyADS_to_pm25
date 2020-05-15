330b103d98f675ad99e5abf805f49be2323c057c
Lucas Henneman <lhenneman@gmail.com>
2020-05-15 11:47:40 -0400

    combine total annual into file

--- RCode/grids_to_counties.R
+++ RCode/grids_to_counties.R
@@ -19,7 +19,7 @@ counties.us <- st_transform( counties.us, p4s)[counties.us$state_name %in% state
 ## ==================================================== ##
 ##          Read in hysplit raster
 ## ==================================================== ##
-grids_to_county <- function( file.in){
+grids_to_counties <- function( file.in){
   print( file.in)
   # read in the file
   in.g <- read.fst( file.in, as.data.table = T)
@@ -37,11 +37,13 @@ grids_to_county <- function( file.in){
   in.r <- rasterFromXYZ( in.g.trim)
   crs( in.r) <- p4s
   names( in.r) <- names(in.g.trim)[!( names( in.g.trim) %in% c( 'x', 'y'))]
+  if( length( names( in.r)) == 1)
+    names( in.r) <- 'hyads'
   
   # convert to sf object
   ncin_spatpoly <- rasterToPolygons( in.r)
   ncin_sf <- st_as_sf( ncin_spatpoly)
-  ncin_sf <- st_transform( ncin_sf, crs( zips))
+  ncin_sf <- st_transform( ncin_sf, crs( counties.us))
   ncin_sf$GID <- 1:nrow( ncin_sf)
   
   # convert to data table and melt
@@ -72,7 +74,7 @@ grids_to_county <- function( file.in){
   counties.pm <- ncin.m.intersect[, .( pm25 = sum( pm25a)), by = .( geoid, uID)]
   
   # cast for smaller file size
-  counties.pm.c <- dcast( zips.pm, geoid ~ uID, value.var = 'pm25')
+  counties.pm.c <- dcast( counties.pm, geoid ~ uID, value.var = 'pm25')
   
   # define output file, write
   file.out <- gsub( 'grids_', 'counties_', file.in)
@@ -92,7 +94,19 @@ grids_to_county <- function( file.in){
 grid.files.yr <- list.files( '/n/zigler_lab/lhenneman/diseperseR/main/output/exp25/',
                              pattern = 'grids_.*\\d{4}\\.fst',
                              full.names = TRUE)
-grids_pm.list <- lapply( grid.files.yr, grids_to_zips)
+grids_pm.list <- lapply( grid.files.yr, grids_to_counties)
+
+# gather the total county files into single file
+count_files_total <- list.files( '/n/zigler_lab/lhenneman/diseperseR/main/output/exp25/',
+                                 pattern = 'counties_pm25_total_\\d{4}\\.fst',
+                                 full.names = TRUE)
+county_allyears <- rbindlist( lapply( 1:20,
+                                      function( n, f, y) {
+                                        read_fst( f[n], as.data.table = T)[, year := y[n]]
+                                      }, count_files_total, 1999:2018))
+
+write.fst( county_allyears, 
+           path = "/n/zigler_lab/lhenneman/diseperseR/main/output/exp25/counties_pm25_total_1999-2018.fst")
 
 ## ==================================================== ##
 ##          Run the function for raw hyads
